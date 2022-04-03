260ed404f5013f1c676cf6c56fa5da3059bfd787
celebrater <tkgm45nbchk@gmail.com>
2022-04-03 14:08:55 +0900

    added fcm_init

---
+++ fcm_init.php
@@ -0,0 +1,7 @@
+<?php
+
+ini_set('display_errors', 1);
+
+define("FCM_PATH", '/home/enin-world/www/files/app/php/fp_common_modules/');
+//define("FCM_PATH", 'J:\\Dropbox\\PC5_cloud\\pg\\xampp\\htdocs\\myapps\\fujimipolis\\files\\app\\php\\fp_common_modules\\');
+define("FCM_HCM", FCM_PATH . 'html_common_module.php');
---
+++ fcm_init.sample.php
@@ -0,0 +1,6 @@
+<?php
+
+ini_set('display_errors', 1);
+
+define("FCM_PATH", '/home/enin-world/www/fp_common_modules/');
+define("FCM_HCM", FCM_PATH . 'html_common_module.php');
